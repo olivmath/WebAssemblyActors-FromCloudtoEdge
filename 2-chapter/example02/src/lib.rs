b6d1d9fc34707a240003ad10938e8019ac7fc797
Lucas Oliveira <lucas.oliveira@lunes.io>
2022-04-18 22:00:19 -0300

    update: 2-chapter

---
+++ 2-chapter/example02/src/lib.rs
@@ -0,0 +1,14 @@
+// Todo
+// add(i32, i32) -> i32
+// sub(i32, i32) -> i32
+// mul(i32, i32) -> i32
+// div(i32, i32) -> i32
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
